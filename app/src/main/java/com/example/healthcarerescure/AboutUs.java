f5cb1cae4eea6c1a11eb979b5b34f526b19b4580
aqilahgiman28 <109784154+aqilahgiman28@users.noreply.github.com>
2022-07-22 19:23:22 +0800

    Update AboutUs.java

--- app/src/main/java/com/example/healthcarerescure/AboutUs.java
+++ app/src/main/java/com/example/healthcarerescure/AboutUs.java
@@ -3,14 +3,22 @@ package com.example.healthcarerescure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import com.example.healthcarerescure.R;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutUs extends AppCompatActivity {
-
+    
+    TextView linkTextView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        
+       
+        
+        linkTextView = findViewById(R.id.linkgit);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 }
